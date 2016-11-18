8baf1e0543fda596ac8083096ce1ffcfeb0df64b
Zyrass <zyrass@outlook.fr>
2016-11-18 10:29:32 +0100

    - Ajout du code JQUERY 	- Ajout de l'image suivez-moi (followme.jpg) 	- ajout du code html pour l'image followme

--- js/myScript.js
+++ js/myScript.js
@@ -9,5 +9,33 @@
 
 $ ( document ).ready(function(){ // Début du code JQUERY
 
+    var animeHover = "easeOutBounce";
+    var animeOutHover = "jswing";
+    
+    var animeSpeedHover = "1000";
+    var animeSpeedOutHover = "300";
+
+    $(".menu ul li a").hover(function() {
+
+        $(this).next("span").animate({
+            opacity : "show",
+            top: "0"
+        },
+        {
+            duration: animeSpeedHover,
+            easing: animeHover
+        })
+    }, function (){
+
+        $(this).next("span").animate({
+            opacity : "hide",
+            top: "20"
+        },
+        {
+            duration : animeSpeedOutHover,
+            easing : animeOutHover
+        })
+    
+    });
 
 }); // Fin du code JQUERY
